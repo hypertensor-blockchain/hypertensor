325a5da4691ecf28a36e773bb976d45831d26dd6
crazydevlegend <crazy010323@gmail.com>
2024-06-26 10:13:45 +0800

    chore (runtime-api): uncommented  function

--- pallets/network/runtime-api/src/lib.rs
+++ pallets/network/runtime-api/src/lib.rs
@@ -5,7 +5,7 @@ sp_api::decl_runtime_apis! {
   pub trait NetworkRuntimeApi {
     fn get_model_peers(model_id: u32) -> Vec<u8>;
     fn get_model_peers_included(model_id: u32) -> Vec<u8>;
-    // fn get_model_peers_submittable(model_id: u32) -> Vec<u8>;
+    fn get_model_peers_submittable(model_id: u32) -> Vec<u8>;
     fn get_model_peers_model_unconfirmed_count(model_id: u32) -> u32;    
   }
 }
